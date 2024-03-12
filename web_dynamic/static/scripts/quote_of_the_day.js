1ee0123a18c305d6d6f7f05a94d4d8275153848b
Houcine Walaq <houcine.walaq@gmai.com>
2024-03-12 12:47:23 +0000

    quote of the day date

--- web_dynamic/static/scripts/quote_of_the_day.js
+++ web_dynamic/static/scripts/quote_of_the_day.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('quote-author').innerText = '- ' + data.author_name; // Adjust if the key is different
         })
         .catch(error => console.error('Failed to fetch the daily quote', error));
+    updateCurrentDateTime();
 });
 
 
@@ -25,4 +26,34 @@ function fetchQuotesByAuthor(authorId, callback) {
         .then(quotesData => {
             callback(quotesData); // Pass the quotes data to the callback
         });
+}
+
+
+
+
+
+function updateCurrentDateTime() {
+    const currentDateElement = document.getElementById('current-date-time');
+
+    // Function to update current date and time every second
+    function updateTime() {
+        const now = new Date();
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            timeZoneName: 'short'
+        };
+        const formattedDateTime = now.toLocaleString('en-US', options);
+        currentDateElement.textContent = formattedDateTime;
+    }
+
+    // Initial call to display current date and time
+    updateTime();
+
+    // Update current date and time every second
+    setInterval(updateTime, 1000);
 }
